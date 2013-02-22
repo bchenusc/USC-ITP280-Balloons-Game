dad8402f5f648b1d390963d402578e2527ff4b98
Matthew Pohlmann <pohlmann@usc.edu>
2013-02-22 15:22:02 -0800

    update script_32_snap

---
+++ OrthelloPractice/Assets/Scripts/script_32_snap.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class script_32_snap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float new_x, new_y;
+		
+		new_x = transform.position.x - transform.position.x % 32f + 16;
+		new_y = transform.position.y - transform.position.y % 32f + 16;
+		
+		transform.position = new Vector2(new_x, new_y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
