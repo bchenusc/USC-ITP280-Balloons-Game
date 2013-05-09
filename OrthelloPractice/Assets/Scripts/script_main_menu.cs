b3f67371a3a4be5aa1dbea3f262bf144c41de3c6
Matthew Pohlmann <pohlmann@usc.edu>
2013-05-08 17:18:27 -0700

    Fixed levels. Changed start button to the actual start button

--- OrthelloPractice/Assets/Scripts/script_main_menu.cs
+++ OrthelloPractice/Assets/Scripts/script_main_menu.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class script_main_menu : MonoBehaviour {
 	private script_persistent persistent;
 	
+	public GUIStyle menuStyle;
+	
 	
 	// Use this for initialization
 	void Start () {
@@ -11,7 +13,7 @@ public class script_main_menu : MonoBehaviour {
 	}
 	
 	void OnGUI() {
-		if (GUI.Button(new Rect(Screen.width/2 - 50, Screen.height/2 - 25, 100, 50), "Start")) {
+		if (GUI.Button(new Rect(Screen.width/2 - Screen.width/8 - Screen.width/15, Screen.height/2  - Screen.height/8 + Screen.width/15, Screen.width/15, Screen.width/15), "", menuStyle)) {
 			persistent.current_level = 1;
 			Application.LoadLevel(1);
 		}
